46aa1d1d4995b925058c99879654250804a491ea
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-02-29 11:23:08 +0100

    feat(order): order side bar

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,5 +1,6 @@
 import { MaxWidthWrapper } from '@/components/max-width-wapper'
-import { ArrowRight } from 'lucide-react'
+import { OrderItem } from '@/components/order'
+import { ArrowRight, CheckCircle2, Printer } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Page() {
@@ -10,7 +11,7 @@ export default function Page() {
           <div className="h-96 w-[34%] space-y-12">
             <div className="w-full space-y-2">
               <h1 className="font-semibold text-[33px]">My orders</h1>
-              <p className="text-base font-medium text-#949494]">
+              <p className="text-base font-medium text-neutral-400">
                 Input your mail to view your order history
               </p>
             </div>
@@ -25,7 +26,7 @@ export default function Page() {
                   className="w-[77px] aspect-square rounded-[21px] object-cover"
                 />
                 <div className="space-y-1">
-                  <h3 className="font-medium text-lg">Order #201</h3>
+                  <h3 className="font-semibold text-lg">Order #201</h3>
                   <div className="w-60">
                     <p className="text-sm text-[#9a9a9a]">
                       Milky Lactation Cookies, Milky Enrich Shakes, Milky
@@ -46,13 +47,94 @@ export default function Page() {
                   className="w-[77px] aspect-square rounded-[21px] object-cover"
                 />
                 <div className="space-y-1">
-                  <h3 className="font-medium text-lg">Order #204</h3>
+                  <h3 className="font-semibold text-lg">Order #204</h3>
                   <div className="w-60">
                     <p className="text-sm text-[#9a9a9a]">Milky storage bag</p>
                   </div>
                 </div>
 
                 <ArrowRight className="w-8 h-8 text-[#9A9A9A] ml-8" />
+              </div>
+            </div>
+          </div>
+          <div className="w-3/5">
+            <div className="w-full flex items-center justify-center ">
+              <div className="w-[70%] flex items-center flex-col space-y-6 pb-20">
+                <Image
+                  src="/log.png"
+                  alt="logo"
+                  width={155}
+                  height={135}
+                  className="object-cover"
+                />
+
+                <div className="flex flex-col items-center space-y-1">
+                  <h3 className="text-[30px] font-bold">Order #201</h3>
+                  <div className="flex items-center space-x-10">
+                    <div className="flex space-x-2 items-center">
+                      <h6>Status:</h6>
+                      <h6 className="text-[#aaaaaa] flex items-center gap-1 text-xs font-medium">
+                        <CheckCircle2 className="text-emerald-500 h-3 w-3" />
+                        Paid
+                      </h6>
+                    </div>
+
+                    <div className="flex space-x-2 items-center">
+                      <h6>Date:</h6>
+                      <h6 className="text-[#aaaaaa] flex items-center gap-1 text-xs font-medium">
+                        1-02-2021
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="w-full h-[43px] rounded-[10px] bg-colorPrimary px-4 text-lg text-white font-semibold flex items-center">
+                  Order Summary
+                </div>
+
+                <div className="w-full">
+                  <OrderItem />
+                </div>
+
+                <div className="w-full pt-12">
+                  <div className="w-full h-[43px] rounded-[10px] bg-colorPrimary px-4 text-lg text-white font-semibold flex items-center">
+                    Account Summary
+                  </div>
+                </div>
+
+                <div className="pt-4 w-full space-y-3">
+                  <div className="flex items-center justify-between">
+                    <h4 className="font-medium text-[17px]">First Name</h4>
+                    <h6 className="text-sm text-[#8d8d8d]">John</h6>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <h4 className="font-medium text-[17px]">Last Name</h4>
+                    <h6 className="text-sm text-[#8d8d8d]">Stone</h6>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <h4 className="font-medium text-[17px]">Email</h4>
+                    <h6 className="text-sm text-[#8d8d8d]">
+                      stonejohn@email.com
+                    </h6>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <h4 className="font-medium text-[17px]">Phone</h4>
+                    <h6 className="text-sm text-[#8d8d8d]">+2349049337263</h6>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <h4 className="font-medium text-[17px]">Address</h4>
+                    <h6 className="text-sm text-[#8d8d8d]">
+                      7 Sdekunle Ajose Street Lekki Lagos Nigeria
+                    </h6>
+                  </div>
+                </div>
+
+                <div className="pt-20 flex items-center space-x-40">
+                  <button className="flex items-center gap-3 text-sm text-[#aaaaaa]">
+                    <Printer className="w-4 h-4" />
+                    Print
+                  </button>
+                </div>
               </div>
             </div>
           </div>
